9fd34f506842d271d6bec1ca6f54003c01ab73b8
Antonin <81852696+Limit-sest@users.noreply.github.com>
2025-10-13 21:55:01 +0200

    Use the button to change text outputted

--- scripts/tty.gd
+++ scripts/tty.gd
@@ -1,7 +1,21 @@
 extends RichTextLabel
-func _ready():
-	text = "Hello from the world of GDScript!"
 
+var enabled_decipher = false
+var print_text = "Cheeeeeeeeeeeeeeeeeese"
 
-func _on_button_pressed() -> void:
-	append_text("\nStarting terminal.")
+func _on_button_toggled(toggled_on: bool) -> void:
+	enabled_decipher = toggled_on
+
+func rand_str(input: String) -> String:
+	var chars = "abcdefghijklmnopqrstuvwxyz-"
+	var random_string = ""
+	for i in range(input.length()):
+		random_string += chars[randi() % chars.length()]
+	return random_string
+
+func _on_timer_timeout() -> void:
+	if enabled_decipher:
+		append_text("\n"+print_text)
+	else:
+		append_text("\n"+rand_str(print_text))
+		
