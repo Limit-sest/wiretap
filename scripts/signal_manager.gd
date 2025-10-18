21cec1105968f367050fca2f97574fb21b07bd04
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-10-18 18:58:54 +0200

    Add decoding based on wires

--- scripts/signal_manager.gd
+++ scripts/signal_manager.gd
@@ -3,10 +3,15 @@ extends Node2D
 @onready var initial_node = %Anthenna/Output
 @onready var encoded_string = CipherUtils.encode_random("Hello world")
 
+var decode_functions = {
+	"Numbers": CipherUtils.decode_numbers,
+	"Morse": CipherUtils.decode_morse,
+	"Caesar": CipherUtils.decode_caesar
+}
+
 func _get_linked_port(first_port: Area2D): # Get output port in the same module
 	var parent = first_port.get_parent()
-	print(parent.name)
-	return parent.find_child("Output")
+	return [parent.find_child("Output"), parent.name]
 
 func _get_next_port(output_port: Area2D): # Get connected port with wire
 	for connection in WiringManager.connections:
@@ -18,8 +23,12 @@ func _on_signal_send_timeout() -> void:
 	var working_string = encoded_string
 	while true:
 		var next_node = _get_next_port(current_node)
-		if !next_node: break
-		current_node = _get_linked_port(next_node)
+		if !next_node: 
+			break
+		elif next_node.get_parent().name == "Display":
+			print(working_string)
+		var linked_port = _get_linked_port(next_node)
+		current_node = linked_port[0]
 		if !current_node: break
+		working_string = decode_functions[linked_port[1]].call(working_string)
 	
-	print(working_string)
