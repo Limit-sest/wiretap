53490e5cd1170460d90a6f7ceb6fd28fe7155635
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-10-18 10:26:13 +0200

    Start working on a global wiring manager

---
+++ scripts/wiring_manager.gd
@@ -0,0 +1,50 @@
+extends Node
+
+var active_line: Line2D 
+var is_drawing = false
+var start_port = null
+
+var connections = []
+
+func _ready():
+	active_line = Line2D.new()
+	active_line.width = 5.0
+	active_line.default_color = Color.from_hsv(randf(), 0.75, 0.9)
+	add_child(active_line)
+
+func start_wire(port_node):
+	if is_drawing:
+		return
+
+	is_drawing = true
+	start_port = port_node
+	active_line.clear_points()
+
+	var start_pos = active_line.to_local(start_port.global_position)
+	var mouse_pos = active_line.to_local(get_viewport().get_mouse_position())
+	active_line.add_point(start_pos)
+	active_line.add_point(mouse_pos)
+
+func _unhandled_input(event):
+	if is_drawing and event is InputEventMouseButton and not event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_RIGHT:
+			var space_state = get_tree().root.get_world_2d().direct_space_state
+			var query = PhysicsPointQueryParameters2D.new()
+			query.position = get_viewport().get_global_mouse_position()
+			query.collide_with_areas = true
+			query.collision_mask = 2 # ports layer
+			
+			var result = space_state.intersect_point(query)
+			if not result.is_empty():
+				var target_port = result[0].collider
+				if target_port.is_in_group("ports") and target_port != start_port:
+					connections.append([start_port, target_port])
+					print("Connection created!")
+			is_drawing = false
+			start_port = null
+			active_line.clear_points()
+			
+func _process(delta):
+	if is_drawing:
+		var mouse_pos = active_line.to_local(get_viewport().get_mouse_position())
+		active_line.set_point_position(1, mouse_pos)
