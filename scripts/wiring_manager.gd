b60ed46be8213ddd147797bfe7e57962468a0713
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-10-18 11:24:22 +0200

    Make wires permanent

--- scripts/wiring_manager.gd
+++ scripts/wiring_manager.gd
@@ -9,11 +9,14 @@ var connections = []
 func _get_world_mouse_position() -> Vector2:
 	return get_viewport().get_canvas_transform().affine_inverse() * get_viewport().get_mouse_position()
 
-func _ready():
+func _new_line() -> void:
 	active_line = Line2D.new()
 	active_line.width = 5.0
 	active_line.default_color = Color.from_hsv(randf(), 0.75, 0.9)
 	add_child(active_line)
+
+func _ready():
+	_new_line()
 
 func start_wire(port_node):
 	if is_drawing:
@@ -39,9 +42,12 @@ func _unhandled_input(event):
 			
 			var result = space_state.intersect_point(query)
 			if not result.is_empty():
+				# When line is dropped over a port
 				var target_port = result[0].collider
 				if target_port.is_in_group("ports") and target_port != start_port:
-					connections.append([start_port, target_port])
+					connections.append([start_port, target_port, active_line])
+					# Create new active line
+					_new_line()
 					print("Connection created!")
 			is_drawing = false
 			start_port = null
