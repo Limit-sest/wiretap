3422dd871f323de35fd47a1a3f0cbd19af89b985
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-02 11:15:07 +0100

    Start rewrite module as control node

--- port.gd
+++ port.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_input_event(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		WiringManager.start_wire(self)
