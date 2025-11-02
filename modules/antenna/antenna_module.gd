2ec90063307c7f92e6728e3209c52c872966f9b8
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-02 17:21:19 +0100

    Add antenna module

---
+++ modules/antenna/antenna_module.gd
@@ -0,0 +1,37 @@
+extends "res://modules/base/base_module.gd"
+
+@onready var beep_sfx: AudioStreamPlayer = %BeepSfx
+var ciphered_text: String
+@export var original_text: String = "test"
+@export var encode_number: int = 1
+
+func _ready() -> void:
+	var cipher_modules = get_tree().get_nodes_in_group('cipher_module')
+	for i in range(encode_number):
+		var module = cipher_modules[randi_range(0, cipher_modules.size() - 1)]
+		cipher_modules.erase(module)
+		ciphered_text = module.encode(ciphered_text, (encode_number - 1) - i)
+
+func _on_signal_send_timeout() -> void:
+	_animate_success()
+	send_signal.emit(ciphered_text, 0)
+
+func _animate_success() -> void:
+	var pins = [self.find_child("pin1"), self.find_child("pin2"), self.find_child("pin3")]
+	
+	beep_sfx.pitch_scale = randf_range(0.9, 1.1)
+	beep_sfx.play()
+	
+	for i in range(3):
+		if pins[i]:
+			for pin in pins:
+				if pin:
+					pin.visible = false
+			pins[i].visible = true
+			await get_tree().process_frame
+			await get_tree().process_frame
+			await get_tree().process_frame
+
+	for pin in pins:
+		if pin:
+			pin.visible = false
